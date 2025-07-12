70913fdd07941a18c64b12dd42c8e348d17e0e39
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-07-12 18:09:53 +0500

    user router check

--- src/app.js
+++ src/app.js
@@ -11,4 +11,11 @@ app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//import routes  
+import userRouter from './routes/user.routes.js '
+app.use('/api/v1/users',userRouter)
+// app.get('/hello', (req, res) => {
+//   res.send('Hello World!');
+// });
 export {app}
